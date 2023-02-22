c0f03122ff03ffa2ff9ce8b978c2eedc8d9df94c
Valerii Dekhtiuk <Dekhtiuk.v.s@gmail.com>
2023-02-22 14:36:59 +0200

    Correct id

--- TelegramBotWebHook/ServiceCosmosDB/MessageInfo.cs
+++ TelegramBotWebHook/ServiceCosmosDB/MessageInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace TelegramBotWebHook.ServiceCosmosDB
 {
     internal class MessageInfo
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
         public string Update_id { get; set; }
         public int Message_id { get; set; }
